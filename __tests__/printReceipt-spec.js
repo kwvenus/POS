c20a1766c6e7fe10164659f887728ab5e724c129
Wong Cho Hing <venus.wong@oocl.com>
2018-11-21 23:41:27 +0800

    loadItemsDetail

--- __tests__/printReceipt-spec.js
+++ __tests__/printReceipt-spec.js
@@ -24,7 +24,23 @@ const itemListWithWeight = [
     {'barcode': 'ITEM000005', 'weight': 1}
 ];
 
+const groupedItem = [
+    {'barcode': 'ITEM000001', 'weight': 5},
+    {'barcode': 'ITEM000003', 'weight': 2},
+    {'barcode': 'ITEM000005', 'weight': 3}
+];
+
+const detailItem = {
+    'Name': 'Sprite', 'Quantity': 5, 'Unit price': 3.00, 'Subtotal': 15.00
+};
+
+
 
 it ('Given item list, when itemWithWeight, then return itemWithWeight[{"barcode": String, "weight": int}]', function(){
     expect(printReceipt.itemWithWeight(item)).toEqual(itemListWithWeight);
+});
+
+
+it ('Given one of the object in groupedItem, when loadItemsDetail, then return detailItemWithPromotion{"Name": String, "Quantity": int, "Unit price": double, "Subtotal": double}', function(){
+    expect(printReceipt.loadItemsDetail(groupedItem[0])).toEqual(detailItem)
 });
--- printReceipt.js
+++ printReceipt.js
@@ -1,3 +1,45 @@
+function loadAllItems() {
+  return [
+    {
+      barcode: 'ITEM000000',
+      name: 'Coca-Cola',
+      unit: 'bottle',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000001',
+      name: 'Sprite',
+      unit: 'bottle',
+      price: 3.00
+    },
+    {
+      barcode: 'ITEM000002',
+      name: 'Apple',
+      unit: 'kg',
+      price: 5.50
+    },
+    {
+      barcode: 'ITEM000003',
+      name: 'Litchi',
+      unit: 'kg',
+      price: 15.00
+    },
+    {
+      barcode: 'ITEM000004',
+      name: 'Battery',
+      unit: 'box',
+      price: 2.00
+    },
+    {
+      barcode: 'ITEM000005',
+      name: 'Noodles',
+      unit: 'bag',
+      price: 4.50
+    }
+  ];
+}
+
+
 function itemWithWeight(item){
     let itemWithWeight = []
     item.map(barcode => barcode.includes('-') ? itemWithWeight.push({'barcode': barcode.slice(0, barcode.indexOf('-')), 'weight': Number(barcode.slice(barcode.indexOf('-')+1, barcode.length))}) : itemWithWeight.push({'barcode': barcode, 'weight': 1}))
@@ -5,6 +47,20 @@ function itemWithWeight(item){
     return itemWithWeight
 }
 
+
+
+function loadItemsDetail(groupedItem){
+
+    let allItems = loadAllItems()
+    for (let item = 0; item < allItems.length; item ++){
+        if(allItems[item].barcode == groupedItem.barcode){
+            return {'Name': allItems[item].name, 'Quantity': groupedItem.weight, 'Unit price': allItems[item].price, 'Subtotal': allItems[item].price * groupedItem.weight}
+        }
+    }
+}
+
 module.exports = {
-    itemWithWeight
+    itemWithWeight,
+    loadItemsDetail
+    //groupItem
 };
